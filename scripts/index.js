3dd82c8f2777d01fe9405686fa381e6b182426af
akazad12 <akazad12@aim.com>
2026-01-19 21:19:57 -0500

    Successful retrieval of weather data based on lat/lon

--- scripts/index.js
+++ scripts/index.js
@@ -5,8 +5,7 @@ async function getLocation(location){
     try {
     const response = await fetch (`https://geocoding-api.open-meteo.com/v1/search?name=${location}&count=1&language=en&format=json`);
     const data = await response.json();
-    console.log(data)
-    const result = data;
+    const result = data.results[0];
 
     return {
         name: result.name,
@@ -19,4 +18,30 @@ async function getLocation(location){
     }
 }
 
-console.log(getLocation('Savannah'))
+// getLocation('Savannah')
+
+async function getWeather(location){
+    const {lat,lon,name} = await getLocation(location);
+    console.log(lat,lon,name)
+    const url = `https://api.open-meteo.com/v1/forecast
+    ?latitude=${lat}
+    &longitude=${lon}
+    &temperature_unit=fahrenheit
+    &current=temperature_2m,precipitation
+    &hourly = temperature_2m, apparent_temperature, precipitation_probability, precipitation,weather_code
+    `.replace(/\s+/g, "");
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data.current);
+    console.log(data.hourly)
+
+}
+async function processData(location){
+    const finalResult = await getWeather(location);
+    console.log(finalResult);
+}
+
+
+
+console.log(getWeather('New York City'))
